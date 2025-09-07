1187dbd7df90886ea467c22acb10f94f2f0f7b81
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-09-07 13:03:00 +0300

    Create product.js
    data file added

---
+++ e-ticaret-app/src/data/product.js
@@ -0,0 +1,34 @@
+const products = [
+  {
+    id: 1,
+    name: "T-shirt",
+    price: 25.0,
+    image: "https://via.placeholder.com/150/0000FF/FFFFFF?text=T-shirt",
+  },
+  {
+    id: 2,
+    name: "Pantolon",
+    price: 50.0,
+    image: "https://via.placeholder.com/150/FFA500/000000?text=Pantolon",
+  },
+  {
+    id: 3,
+    name: "Ayakkabı",
+    price: 75.0,
+    image: "https://via.placeholder.com/150/808080/FFFFFF?text=Ayakkabi",
+  },
+  {
+    id: 4,
+    name: "Şapka",
+    price: 15.0,
+    image: "https://via.placeholder.com/150/FFC0CB/000000?text=Sapka",
+  },
+  {
+    id: 5,
+    name: "Ceket",
+    price: 120.0,
+    image: "https://via.placeholder.com/150/FFFF00/000000?text=Ceket",
+  },
+];
+
+export default products;
